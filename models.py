396df3f07a73f4af909be403dff1c73cbde37903
Joshua Dias <joshuadiasbf@gmail.com>
2025-01-05 11:25:52 -0300

    Class Create -> models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Interaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_input = db.Column(db.Text, nullable=False)
+    response = db.Column(db.Text, nullable=False)
+    suggestions = db.Column(db.Text)
+    sentiment = db.Column(db.String(20))
+    grammar_corrections = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
